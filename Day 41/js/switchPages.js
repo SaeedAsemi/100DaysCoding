ba1ef649b75b487e46ab74b9c1928f3b4ba33b25
Saeed Asemi <saeed.asemi1@gmail.com>
2021-02-17 23:16:37 +0330

    I move Shopify contents to the JS variables and change the pages without refreshing

--- Day 41/js/switchPages.js
+++ Day 41/js/switchPages.js
@@ -1,40 +1,21 @@
 import { ls0, ls1, ls2, ls3, ls4, ls5, ls6 } from "./pages.js";
-
 let main = document.querySelector(".main");
-let list0 = document.querySelector(".main-nav-logo");
-let list1 = document.querySelector(".list1");
-let list2 = document.querySelector(".list2");
-let list3 = document.querySelector(".list3");
-let list4 = document.querySelector(".list4");
-let list5 = document.querySelector(".list5");
-let list6 = document.querySelector(".list6");
+const changePage = document.querySelectorAll(".change-page");
 
-main.innerHTML = ls0;
+const pages = {
+  0: ls0,
+  1: ls1,
+  2: ls2,
+  3: ls3,
+  4: ls4,
+  5: ls5,
+  6: ls6,
+};
 
-list0.addEventListener("click", () => {
-  main.innerHTML = ls0;
-});
+main.innerHTML = pages[0];
 
-list1.addEventListener("click", () => {
-  main.innerHTML = ls1;
-});
-
-list2.addEventListener("click", () => {
-  main.innerHTML = ls2;
-});
-
-list3.addEventListener("click", () => {
-  main.innerHTML = ls3;
-});
-
-list4.addEventListener("click", () => {
-  main.innerHTML = ls4;
-});
-
-list5.addEventListener("click", () => {
-  main.innerHTML = ls5;
-});
-
-list6.addEventListener("click", () => {
-  main.innerHTML = ls6;
+changePage.forEach((list, index) => {
+  list.addEventListener("click", () => {
+    main.innerHTML = pages[index];
+  });
 });
